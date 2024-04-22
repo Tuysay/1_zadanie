43e94abf520888497efa67e454ca104d372aaaec
gr423_gamse <misha.gapeev.04@gmail.com>
2024-04-22 09:44:49 +0700

    батоны

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        cart: 0
     }
 })
+
+
+// image: "./assets/vmSocks-green-onWhite.jpg",
+// image: "./assets/vmSocks-blue-onWhite.jpg",
